4ab23d3f72b7f16d3ce36148d3fc438245b06f26
Viet PHH <phhoangviet.hoctap@gmail.com>
2019-11-06 20:41:22 +0700

    update

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use ('/auth/upload-avatar',uploadAvatar);
 app.use('/login/google',googleLogin);
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "localhost:8080"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://api-midterm.herokuapp.com/"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
